7aa5f48501753d3582ea43014722cff07a787d72
Jayda Fortney <bookkitti724@yahoo.com>
2024-10-16 19:27:10 -0700

    starting code for week 11 code discussion challenge

---
+++ w11/render.js
@@ -0,0 +1,76 @@
+import { FORM, TBL } from "./global.js";
+import { saveLS } from "./storage.js";
+
+const renderTblHead = () => {
+  const table = document.createElement("table");
+    const thead = document.createElement("thead");
+    const tr = document.createElement("tr");
+    const headingTextArr = ["Name", "HouseHold", "HouseSize", "Footprint", "Actions"];
+    headingTextArr.forEach((text) => {
+      const th = document.createElement("th");
+      th.textContent = text;
+      tr.appendChild(th);
+    });
+    thead.appendChild(tr);
+    table.appendChild(thead);
+    return table
+}
+
+
+const onUpdate = (index, data) => {
+  data.splice(index, 1);
+  saveLS(data);
+  renderTbl(data); 
+}
+
+  const renderTblBttns = (index, data) => {
+    const td = document.createElement("td");
+    const btnEdit = document.createElement("button");
+    btnEdit.textContent = "Edit"
+    const btnDel = document.createElement("button");
+    btnDel.textContent = "Del"
+    td.appendChild(btnEdit);
+    td.appendChild(btnDel);
+    btnDel.addEventListener("click", e => {
+    onUpdate(index, data);
+    })
+    btnEdit.addEventListener("click", e => {
+      FORM[1].value = String(data[index].first);
+      FORM[2].value = String(data[index].last);
+      FORM[3].value = String(data[index].houseMembers);
+      FORM[4].value = String(data[index].houseSize); //adjusted each of these values for editing
+      onUpdate(index, data); 
+    })
+    return td
+  }
+
+  const renderTblRow = data => {
+  const tbody = document.createElement("tbody")
+    data.forEach((obj, index) => {
+      console.log(index);
+      const tr = document.createElement("tr");
+      for(const [key, value] of Object.entries(obj)){
+        if (key === "first" || key === "houseMembers" || key === "houseSize" || key === "total") { //adjusted each of these values for rendering
+          const td = document.createElement("td");
+          td.textContent = value;
+          tr.appendChild(td);
+        }
+    };
+    const td = renderTblBttns(index, data);
+    tr.appendChild(td);
+    tbody.appendChild(tr);
+    });
+  return tbody
+  }
+  
+  const renderTbl = data => {
+    TBL.innerHTML = "";
+    if (data.length !== 0) {
+      const table = renderTblHead();
+      const tbody = renderTblRow(data);
+      table.appendChild(tbody);
+      TBL.appendChild(table);
+    }
+  }
+
+  export {renderTbl};
