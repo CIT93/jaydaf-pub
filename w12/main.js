c7ef70cc369c4949cd2f0d15d5aa1bce7d5d7638
Jayda Fortney <bookkitti724@yahoo.com>
2024-10-24 15:13:36 -0700

    updated code with solution video

--- w12/main.js
+++ w12/main.js
@@ -1,36 +1,24 @@
-const repArr = [];
 const FORM = document.getElementById("form");
 
-const start = (exerciseType, repNum, workoutTime) => {
-    repArr.push({
-        exerciseType: exerciseType,
-        repNum: repNum,
-        workoutTime: workoutTime
-    })
+const startWorkout = (exerciseType, repNum, workoutTime, fn) => {
+    fn(`Start ${exerciseType} <> Goal reps is ${repNum} <> Complete in ${workoutTime} seconds!`, "p")
+    setTimeout(() => {
+        fn(`Stop ${exerciseType}`, "h1")
+    }, workoutTime * 1000);
 }
 
-const displayOutput = () => {
-  for (const obj of repArr) {
-    console.log(obj);
+const displayOutput = (message, el) => {
     const output = document.getElementById("output");
-    const newH2 = document.createElement("h2");
-    const newP = document.createElement("p");
-    output.innerHTML = "Submit to Start!";
-    newP.textContent = `Start ${obj.exerciseType} <> Goal reps is ${obj.repNum}`;
-    newH2.textContent = `Stop ${obj.exerciseType}`;
-    output.appendChild(newP);
-    setTimeout(() => {
-        output.appendChild(newH2);
-    }, obj.workoutTime * 1000);
-  }
-};
+    const newEl = document.createElement(el);
+    newEl.textContent = message;
+    output.appendChild(newEl);
+  };
 
 FORM.addEventListener("submit", (e) => {
     e.preventDefault();
     const exerciseType = FORM.exerciseType.value;
     const repNum = parseInt(FORM.repNum.value);
     const workoutTime = parseInt(FORM.workoutTime.value);
-    start(exerciseType, repNum, workoutTime);
-    displayOutput();
+    startWorkout(exerciseType, repNum, workoutTime, displayOutput);
     FORM.reset();
   });
